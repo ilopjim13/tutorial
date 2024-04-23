1d45847c32ea0e7c4c7687e840b6cb455a351a9f
ivanjim13 <ivangorrion57@gmail.com>
2024-04-24 01:57:05 +0200

    login terminado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,49 +8,44 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.*
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Login") }
-    var consumedText by remember { mutableStateOf(0) }
+    var usuario by remember { mutableStateOf("") }
+    var contra by remember { mutableStateOf("") }
 
     MaterialTheme {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            //verticalArrangement = Arrangement.spacedBy(50.dp),
+            verticalArrangement = Arrangement.spacedBy(15.dp, alignment = Alignment.CenterVertically),
             modifier = Modifier.fillMaxSize()
         ) {
 
-            Box(
-                modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 40.dp)
-                    .border(2.dp, Color.LightGray)
-                    .width(200.dp)
-            ) {
-                Text("", modifier = Modifier.padding(15.dp))
-            }
-
-            Box (
-                modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 55.dp)
-                    .border(2.dp, Color.LightGray)
-                    .width(200.dp)
-            ) {
-                Text("", modifier = Modifier.padding(15.dp))
-            }
+            OutlinedTextField(
+                value = usuario,
+                onValueChange = {usuario = it},
+                label = {Text("Usuario")}
+            )
+            OutlinedTextField(
+                value = contra,
+                onValueChange = {contra = it},
+                label = {Text("Contraseña")},
+                visualTransformation = PasswordVisualTransformation()
+            )
 
             Button(onClick = {
-
+                usuario = ""
+                contra = ""
             }) {
-                Text(text)
+                Text("Login")
             }
         }
 
