88510a7d45e676f0338a6715a52501641318b187
ivanjim13 <ivangorrion57@gmail.com>
2024-04-24 03:00:52 +0200

    login terminado del todo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,4 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
@@ -9,18 +8,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.*
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.ui.text.input.VisualTransformation
 
 @Composable
 @Preview
 fun App() {
     var usuario by remember { mutableStateOf("") }
     var contra by remember { mutableStateOf("") }
+    var visibilidad by remember { mutableStateOf(false) }
+
 
     MaterialTheme {
         Column(
@@ -38,7 +41,16 @@ fun App() {
                 value = contra,
                 onValueChange = {contra = it},
                 label = {Text("Contraseña")},
-                visualTransformation = PasswordVisualTransformation()
+                visualTransformation = if (visibilidad) VisualTransformation.None else PasswordVisualTransformation(),
+                trailingIcon = {
+                    IconToggleButton(checked = visibilidad, onCheckedChange = {visibilidad = it}) {
+                        Icon(
+                            imageVector = if(visibilidad) Icons.Default.VisibilityOff else Icons.Default.Visibility,
+                            contentDescription = ""
+                        )
+                    }
+                }
+
             )
 
             Button(onClick = {
